6894cab9151990d44f2489cdd2742fe4a9c1fc5f
Karthikeyan <innovatorforindia@gmail.com>
2013-11-27 14:29:49 +0530

    folder Name Changed

---
+++ CucumberExample/src/main/java/com/demo/automation/GoogleSearchPage.java
@@ -0,0 +1,76 @@
+package main.java.com.demo.automation;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+
+
+
+
+
+
+public class GoogleSearchPage {
+	
+	private final WebDriver driver;
+
+	@FindBy(name = "q")
+	private WebElement searchBox;
+	
+	
+	@FindBy(name = "btnG")
+	private WebElement searchButton;
+	
+	
+	@FindBy(linkText = "Selenium - Web Browser Automation")
+	private WebElement result;
+	
+	
+	
+	
+	
+	public GoogleSearchPage(WebDriver driver)
+	{
+		this.driver = driver;
+		
+	}
+	
+	
+	
+	public GoogleSearchPage clickSearchButton()
+	{
+		searchButton.click();
+		return PageFactory.initElements(driver, this.getClass());
+
+	}
+	
+	public void enterSearchTerm(String searchTerm)
+	{
+		/*try {
+			Thread.sleep(10000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}*/
+		
+		
+		searchBox.click();
+		searchBox.clear();
+		searchBox.sendKeys(searchTerm);
+	}
+	
+	public boolean isResultDisplayed()
+	{
+		try {
+		Thread.sleep(5000);
+	} catch (InterruptedException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+		return result.isDisplayed();
+	}
+	
+
+}
---
+++ CucumberExample/src/test/java/com/demo/automation/SimpleTextMunger_Test.java
@@ -0,0 +1,10 @@
+package test.java.com.demo.automation;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@Cucumber.Options(format = {"pretty"})
+public class SimpleTextMunger_Test {
+}
